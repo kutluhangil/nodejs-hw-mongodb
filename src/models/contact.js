0c25ebaad1bf7c47f7cc5bce4161dec765cf3acc
kutluhangil <kutluhangul@windowslive.com>
2026-03-12 01:51:37 +0300

    add contact model

---
+++ src/models/contact.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      minlength: 3,
+      maxlength: 20,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Contact = mongoose.model('contacts', contactSchema);
+
+module.exports = Contact;
